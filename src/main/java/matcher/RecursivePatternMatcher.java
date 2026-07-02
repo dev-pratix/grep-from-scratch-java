a0ad781d817db0ee7595a9077752b7080a75e774
pratik.bhadale <pratikbhadale633@gmail.com>
2026-07-02 08:55:05 +0530

    codecrafters submit [skip ci]

--- src/main/java/matcher/RecursivePatternMatcher.java
+++ src/main/java/matcher/RecursivePatternMatcher.java
@@ -1,46 +1,127 @@
 package matcher;
 
 import model.Token;
+import model.TokenType;
 
-public class RecursivePatternMatcher {
+public final class RecursivePatternMatcher {
 
-    public static boolean matches(String input, String pattern) {
-        return match(input, 0, pattern, 0);
+    private RecursivePatternMatcher() {
     }
 
-    public static boolean match(String input, int idx, String pattern, int pIdx) {
+    public static boolean matches(String input, String pattern) {
+        Token firstToken = TokenReader.read(pattern, 0);
 
-        if (pIdx == pattern.length()) {
-            return idx == input.length();
+        if (firstToken.getType() == TokenType.START_ANCHOR) {
+            return doesRemainingPatternMatchHere(input, 0, pattern, 0);
         }
 
-        Token currentToken = TokenReader.read(pattern, pIdx);
+        // s o here we will start the input val per increment and pattern will always start 0
+        for (int start = 0; start < input.length(); start++) {
+            if (doesRemainingPatternMatchHere(
+                    input,
+                    start,
+                    pattern,
+                    0)) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean doesRemainingPatternMatchHere(
+            String input,
+            int inputIdx,
+            String pattern,
+            int patternIdx) {
+
+        if (patternIdx == pattern.length()) {
+            return true;
+        }
+
+        Token currentToken = TokenReader.read(pattern, patternIdx);
+
+        int nextPatternIdx = patternIdx + currentToken.getLength();
+
+        Token nextToken = null;
+        if (nextPatternIdx < pattern.length()) {
+            nextToken = TokenReader.read(pattern, nextPatternIdx);
+        }
+
         return switch (currentToken.getType()) {
             case START_ANCHOR -> {
-                if (idx != 0) {
-                    yield false;
-                }
-                yield match(input, idx, pattern, pIdx + currentToken.getLength());
-            }
-            case END_ANCHOR -> {
-                if (idx != input.length()) {
+                if (inputIdx != 0) {
                     yield false;
                 }
 
-                yield match(input, idx, pattern, pIdx + currentToken.getLength());
+                yield doesRemainingPatternMatchHere(
+                        input,
+                        inputIdx,
+                        pattern,
+                        nextPatternIdx
+                );
             }
+            case END_ANCHOR -> {
+                if (inputIdx != input.length()) {
+                    yield false;
+                }
+                yield doesRemainingPatternMatchHere(
+                        input,
+                        inputIdx,
+                        pattern,
+                        nextPatternIdx
+                );
+            }
+
             default -> {
-                if (idx == input.length()) {
+                if (inputIdx == input.length()) {
                     yield false;
                 }
 
                 if (!CharacterMatcher.matches(
-                        input.charAt(idx),
+                        input.charAt(inputIdx),
                         currentToken)) {
+                    yield false;
+                }
+
+                if (nextToken != null
+                        && nextToken.getType() == TokenType.PLUS) {
+                    int remainingPatternIdx =
+                            nextPatternIdx + nextToken.getLength();
+
+                    int candidateStartIdx = inputIdx + 1;
+
+                    while (true) {
+
+                        if (doesRemainingPatternMatchHere(
+                                input,
+                                candidateStartIdx,
+                                pattern,
+                                remainingPatternIdx)) {
+
+                            yield true;
+                        }
+
+                        if (candidateStartIdx >= input.length()
+                                || !CharacterMatcher.matches(
+                                input.charAt(candidateStartIdx),
+                                currentToken)) {
+
+                            break;
+                        }
+
+                        candidateStartIdx++;
+                    }
 
                     yield false;
                 }
-                yield match(input, idx + 1, pattern, pIdx + currentToken.getLength());
+
+                yield doesRemainingPatternMatchHere(
+                        input,
+                        inputIdx + 1,
+                        pattern,
+                        nextPatternIdx
+                );
             }
         };
     }
